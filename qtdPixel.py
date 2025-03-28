0ab69b993b39a5b450e1d0cce1737e38c18cbf3f
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2025-03-28 17:52:08 -0300

    algoritmo que conta os pixels de uma imagem

---
+++ qtdPixel.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import numpy as np
+
+def contar_pixels_figura(caminho_imagem):
+    
+    imagem = Image.open(caminho_imagem).convert("L")
+    QL, QC = imagem.size
+    total_pixels = QL * QC
+    figura_pixels = 0
+    imagem = np.asarray(imagem)
+    #print(imagem)
+    for i in range(QL):
+        for j in range(QC):
+            if imagem[i][j] == 0:
+                figura_pixels += 1
+    
+    
+    return figura_pixels
+
+
+caminho = "img/pokeball.jpg"
+print(f"Total de pixels da figura: {contar_pixels_figura(caminho)}")
