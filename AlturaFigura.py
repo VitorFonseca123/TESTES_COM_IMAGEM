aba0a64292fa1f6b9bdf3818b0f1cb7193bfa90d
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2025-04-02 19:05:38 -0300

    cria metodo alternativo para altura

--- AlturaFigura.py
+++ AlturaFigura.py
@@ -19,6 +19,30 @@ def AlturaDaFigura(caminho_imagem):
     
     return alturaPixels
 
+def AlturaDaFiguraAlternativo(caminho_imagem):
+    imagem = Image.open(caminho_imagem).convert("L")
+    QC, QL = imagem.size # QC = largura, QL = altura
+    total_pixels = QL * QC
+    alturaPixels = 0
+    alturaAuxiliar = 0
+
+    imagem = np.asarray(imagem)
+    print("Total de pixels da altura ", QL)
+    indiceColuna = 0
+    
+    while alturaAuxiliar < QL:
+        if indiceColuna < QC and imagem[alturaAuxiliar, indiceColuna] < 100:
+            alturaPixels += 1
+            alturaAuxiliar += 1
+            indiceColuna = 0  
+        elif indiceColuna >= QC - 1:  
+            alturaAuxiliar += 1
+            indiceColuna = 0  
+        else:
+            indiceColuna += 1 
+
+    return alturaPixels
+            
 
 caminho = "img/ponto.png"
-print(f"Total de pixels da figura: {AlturaDaFigura(caminho)}")
+print(f"Total de pixels da altura da figura: {AlturaDaFiguraAlternativo(caminho)}")
